cc081bf9bff0d138a10e48753f8b7c88fa6bd071
bartekar <55534106+bartekar@users.noreply.github.com>
2022-11-22 02:47:03 +0100

    [ADD] more unit tests for other bounding boxes

--- DataTests.cpp
+++ DataTests.cpp
@@ -18,7 +18,7 @@ struct Box
 		boxDiamond = new BBox(0.0f, 0.0f, 2.0f, 2.0f, PI / 4.0f);
 		boxRect = new BBox(0.0f, 0.0f, 10.0f, 1.0f, 0.0f);
 		boxMoved = new BBox(-3.0f, 3.0f, 1.0f, 1.0f, 0.0f);
-		boxRandom = new BBox(5.0f, 5.0f, 8.0f, 4.0f, PI / 8.0f);
+		boxRandom = new BBox(5.0f, 5.0f, 8.0f, 4.0f, PI / 8.0f); // pi/8 -> 22.5 degree
 	}
 
 	virtual void TearDown() override
@@ -117,37 +117,41 @@ TEST_F(Box, rectTest) {
 	ASSERT_TRUE(boxRect->containsPoint(p)) << "left lower corner case";
 	delete p;
 }
-/*
-TEST_F(Box, containingPoints) {
-	float exp_area = 4.0f;
-	EXPECT_EQ(exp_area, boxDiamond->area()) << "area test";
 
-	Point* p = new Point(0.5f, 0.5f);
-	ASSERT_TRUE(boxDiamond->containsPoint(p)) << "contains point 0";
+TEST_F(Box, movedBoxTests) {
+	float exp_area = 1.0f;
+	EXPECT_EQ(exp_area, boxMoved->area()) << "area test";
+
+	Point* p = new Point(-3.0f, 3.0f);
+	ASSERT_TRUE(boxMoved->containsPoint(p)) << "contains point";
 	delete p;
 
-	p = new Point(1.0f, 0.0f);
-	ASSERT_TRUE(boxDiamond->containsPoint(p)) << "contains point 1";
+	p = new Point(3.0f, -3.0f);
+	ASSERT_FALSE(boxMoved->containsPoint(p)) << "not contain";
 	delete p;
 
-	p = new Point(1.42f, 0.0f);
-	ASSERT_FALSE(boxDiamond->containsPoint(p)) << "contains point 2";
+	p = new Point(-4.0f, 2.0f);
+	ASSERT_FALSE(boxMoved->containsPoint(p)) << "does not contain point because height and width are actually halved";
+	delete p;
+
+	p = new Point(-3.5f, 2.5f);
+	ASSERT_TRUE(boxMoved->containsPoint(p)) << "touches edge";
 	delete p;
 }
 
-TEST_F(Box, containingPoints) {
-	float exp_area = 4.0f;
-	EXPECT_EQ(exp_area, boxDiamond->area()) << "area test";
+TEST_F(Box, allParamsBox) {
+	float exp_area = 32.0f;
+	EXPECT_EQ(exp_area, boxRandom->area()) << "area test";
 
-	Point* p = new Point(0.5f, 0.5f);
-	ASSERT_TRUE(boxDiamond->containsPoint(p)) << "contains point 0";
+	Point* p = new Point(5.0f, 7.0f);
+	ASSERT_TRUE(boxRandom->containsPoint(p)) << "contains point";
 	delete p;
 
-	p = new Point(1.0f, 0.0f);
-	ASSERT_TRUE(boxDiamond->containsPoint(p)) << "contains point 1";
+	p = new Point(7.0f, 3.0f);
+	ASSERT_FALSE(boxRandom->containsPoint(p)) << "contains point 1";
 	delete p;
 
-	p = new Point(1.42f, 0.0f);
-	ASSERT_FALSE(boxDiamond->containsPoint(p)) << "contains point 2";
+	p = new Point(1.0f, 5.0f);
+	ASSERT_TRUE(boxRandom->containsPoint(p)) << "contains point 2";
 	delete p;
-}*/
+}
