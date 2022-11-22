f8f482725f78c40efc8644f4609060daf101c26f
bartekar <55534106+bartekar@users.noreply.github.com>
2022-11-22 02:35:07 +0100

    [ADD] proper fixtures, first unit tests

--- DataTests.cpp
+++ DataTests.cpp
@@ -16,9 +16,9 @@ struct Box
 	virtual void SetUp() override
 	{
 		boxDiamond = new BBox(0.0f, 0.0f, 2.0f, 2.0f, PI / 4.0f);
-		boxRect = new BBox(0.0f, 0.0f, 2.0f, 2.0f, PI / 4.0f);
-		boxMoved = new BBox(0.0f, 0.0f, 2.0f, 2.0f, PI / 4.0f);
-		boxRandom = new BBox(0.0f, 0.0f, 2.0f, 2.0f, PI / 4.0f);
+		boxRect = new BBox(0.0f, 0.0f, 10.0f, 1.0f, 0.0f);
+		boxMoved = new BBox(-3.0f, 3.0f, 1.0f, 1.0f, 0.0f);
+		boxRandom = new BBox(5.0f, 5.0f, 8.0f, 4.0f, PI / 8.0f);
 	}
 
 	virtual void TearDown() override
@@ -80,11 +80,74 @@ TEST(BBoxTest, area) {
 	delete box;
 }
 
-TEST_F(Box, containingPoint) {
+TEST_F(Box, diamondTest) {
 	float exp_area = 4.0f;
 	EXPECT_EQ(exp_area, boxDiamond->area()) << "area test";
 
 	Point* p = new Point(0.5f, 0.5f);
-	ASSERT_TRUE(boxDiamond->containsPoint(p));
+	ASSERT_TRUE(boxDiamond->containsPoint(p)) << "contains point";
+	delete p;
+
+	p = new Point(1.0f, 0.0f);
+	ASSERT_TRUE(boxDiamond->containsPoint(p)) << "contains point";
+	delete p;
+
+	p = new Point(1.42f, 0.0f);
+	ASSERT_FALSE(boxDiamond->containsPoint(p)) << "almost contains point";
 	delete p;
 }
+
+TEST_F(Box, rectTest) {
+	float exp_area = 10.0f;
+	EXPECT_EQ(exp_area, boxRect->area()) << "area test";
+
+	Point* p = new Point(0.5f, 0.5f);
+	ASSERT_TRUE(boxRect->containsPoint(p)) << "contains point on top edge";
+	delete p;
+
+	p = new Point(4.0f, 0.0f);
+	ASSERT_TRUE(boxRect->containsPoint(p)) << "contains point";
+	delete p;
+
+	p = new Point(0.0f, 4.0f);
+	ASSERT_FALSE(boxRect->containsPoint(p)) << "point outside the rect";
+	delete p;
+
+        p = new Point(-5.0f, -0.5f);
+	ASSERT_TRUE(boxRect->containsPoint(p)) << "left lower corner case";
+	delete p;
+}
+/*
+TEST_F(Box, containingPoints) {
+	float exp_area = 4.0f;
+	EXPECT_EQ(exp_area, boxDiamond->area()) << "area test";
+
+	Point* p = new Point(0.5f, 0.5f);
+	ASSERT_TRUE(boxDiamond->containsPoint(p)) << "contains point 0";
+	delete p;
+
+	p = new Point(1.0f, 0.0f);
+	ASSERT_TRUE(boxDiamond->containsPoint(p)) << "contains point 1";
+	delete p;
+
+	p = new Point(1.42f, 0.0f);
+	ASSERT_FALSE(boxDiamond->containsPoint(p)) << "contains point 2";
+	delete p;
+}
+
+TEST_F(Box, containingPoints) {
+	float exp_area = 4.0f;
+	EXPECT_EQ(exp_area, boxDiamond->area()) << "area test";
+
+	Point* p = new Point(0.5f, 0.5f);
+	ASSERT_TRUE(boxDiamond->containsPoint(p)) << "contains point 0";
+	delete p;
+
+	p = new Point(1.0f, 0.0f);
+	ASSERT_TRUE(boxDiamond->containsPoint(p)) << "contains point 1";
+	delete p;
+
+	p = new Point(1.42f, 0.0f);
+	ASSERT_FALSE(boxDiamond->containsPoint(p)) << "contains point 2";
+	delete p;
+}*/
